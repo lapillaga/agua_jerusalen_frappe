a9f20e7ed7d9f404d67062490a4a4b4872a3cb64
Luis Pillaga <luispillaga@Luiss-MacBook-Air.local>
2022-12-25 14:05:01 -0500

    ADD: More fields to jagua jerusalen

---
+++ agua_jerusalen_frappe/junta_de_agua_potable_jerusalen/doctype/periodo_pago/periodo_pago.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Luis Pillaga and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Periodo Pago', {
+	// refresh: function(frm) {
+
+	// }
+});
