b381fe9d1a736362f402ab9f0b7f153139cd0dac
Luis Pillaga <luispillaga@Luiss-MacBook-Air.local>
2022-12-26 15:34:03 -0500

    Add excess consume

--- agua_jerusalen_frappe/junta_de_agua_potable_jerusalen/doctype/medicion/medicion.py
+++ agua_jerusalen_frappe/junta_de_agua_potable_jerusalen/doctype/medicion/medicion.py
@@ -4,9 +4,40 @@
 import frappe
 from frappe.model.document import Document
 from frappe import _
+from frappe.utils import now
 
 
 class Medicion(Document):
+
+	def before_submit(self):
+		"""Create water bill"""
+		import pdb
+		pdb.set_trace()
+		customer = frappe.get_value("Medidor", self.customer.name, 'customer')
+		print(self)
+		print(customer)
+		print(self.water_meter)
+		frappe.throw(_('En desarrollo'))
+
+	# water_bill = frappe.new_doc('Planilla')
+		# water_bill.customer = self.customer
+		# water_bill.issue_date = now()
+		# water_bill.start_date =
+		# water_bill.days_billed =
+		# water_bill.paid = False
+		# water_bill.base_price =
+		# water_bill.amount =
+		# water_bill.water_meter = self.water_meter
+		# water_bill.expiration_date =
+		# water_bill.end_date =
+		# water_bill.rate =
+		# water_bill.billed = False
+		# water_bill.penalty_price =
+		# water_bill.excess_amount =
+		# water_bill.total =
+		# water_bill.save()
+
+
 	@frappe.whitelist()
 	def get_excess_consume(self, throw_if_missing=False, **kwargs):
 		"""Calculate the excess consume"""
