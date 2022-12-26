e465d4a868f744d32fdfc48d3624846b8c52f5c4
Luis Pillaga <luispillaga@Luiss-MacBook-Air.local>
2022-12-26 17:37:05 -0500

    Completo planillas

--- agua_jerusalen_frappe/junta_de_agua_potable_jerusalen/doctype/medicion/medicion.py
+++ agua_jerusalen_frappe/junta_de_agua_potable_jerusalen/doctype/medicion/medicion.py
@@ -5,53 +5,61 @@ import frappe
 from frappe.model.document import Document
 from frappe import _
 from frappe.utils import now
+from frappe.utils import add_days
 
 
 class Medicion(Document):
 
-	def before_submit(self):
-		"""Create water bill"""
-		import pdb
-		pdb.set_trace()
-		customer = frappe.get_value("Medidor", self.customer.name, 'customer')
-		print(self)
-		print(customer)
-		print(self.water_meter)
-		frappe.throw(_('En desarrollo'))
+    def before_submit(self):
+        """Create water bill and also update water meter reading"""
+        water_meter = frappe.get_doc('Medidor', self.water_meter)
+        water_meter.actual_reading = self.actual_reading
+        water_meter.save()
 
-	# water_bill = frappe.new_doc('Planilla')
-		# water_bill.customer = self.customer
-		# water_bill.issue_date = now()
-		# water_bill.start_date =
-		# water_bill.days_billed =
-		# water_bill.paid = False
-		# water_bill.base_price =
-		# water_bill.amount =
-		# water_bill.water_meter = self.water_meter
-		# water_bill.expiration_date =
-		# water_bill.end_date =
-		# water_bill.rate =
-		# water_bill.billed = False
-		# water_bill.penalty_price =
-		# water_bill.excess_amount =
-		# water_bill.total =
-		# water_bill.save()
+        water_bill = frappe.new_doc('Planilla')
+        customer = frappe.get_doc('Customer', self.customer)
+        water_bill.customer = customer.name
 
+        period = frappe.get_doc('Periodo Pago', self.period)
+        water_bill.issue_date = now()
+        water_bill.start_date = period.start_date
+        water_bill.days_billed = period.days_billed
+        water_bill.paid = False
 
-	@frappe.whitelist()
-	def get_excess_consume(self, throw_if_missing=False, **kwargs):
-		"""Calculate the excess consume"""
-		water_meter_name: str = kwargs.get('water_meter_name')
-		total_consume: int = kwargs.get('total_consume')
+        rate = frappe.get_doc('Tarifa', water_meter.rate)
+        water_bill.base_price = rate.amount
+        water_bill.water_meter = water_meter.name
 
-		rate_name = frappe.get_value("Medidor", water_meter_name, 'rate')
-		rate = frappe.get_doc('Tarifa', rate_name)
+        expiration_date = add_days(now(), rate.grace_days)
+        water_bill.expiration_date = expiration_date
+        water_bill.end_date = period.end_date
+        water_bill.rate = rate.name
+        water_bill.billed = False
+        water_bill.penalty_price = rate.penalty
 
-		if not rate and throw_if_missing:
-			frappe.throw(_('El medidor seleccionado no tiene una tarifa asignada'))
+        excess_amount = self.excess_consume * rate.penalty
+        water_bill.excess_amount = excess_amount
 
-		excess_consume = 0
-		if total_consume > rate.max:
-			excess_consume = total_consume - rate.max
+        total = excess_amount + rate.amount
+        water_bill.total = total
 
-		return excess_consume
+        water_bill.measurement = self.name
+        water_bill.save()
+
+    @frappe.whitelist()
+    def get_excess_consume(self, throw_if_missing=False, **kwargs):
+        """Calculate the excess consume"""
+        water_meter_name: str = kwargs.get('water_meter_name')
+        total_consume: int = kwargs.get('total_consume')
+
+        rate_name = frappe.get_value("Medidor", water_meter_name, 'rate')
+        rate = frappe.get_doc('Tarifa', rate_name)
+
+        if not rate and throw_if_missing:
+            frappe.throw(_('El medidor seleccionado no tiene una tarifa asignada'))
+
+        excess_consume = 0
+        if total_consume > rate.max:
+            excess_consume = total_consume - rate.max
+
+        return excess_consume
