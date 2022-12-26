64229b94755b2c13963ceffb8e3d98d84754d8fc
Luis Pillaga <luispillaga@Luiss-MacBook-Air.local>
2022-12-25 20:10:05 -0500

    ADD: More fields to jagua jerusalen

--- agua_jerusalen_frappe/junta_de_agua_potable_jerusalen/doctype/periodo_pago/periodo_pago.js
+++ agua_jerusalen_frappe/junta_de_agua_potable_jerusalen/doctype/periodo_pago/periodo_pago.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2022, Luis Pillaga and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Periodo Pago', {
-	// refresh: function(frm) {
+frappe.ui.form.on('Medicion', {
+    measurements_add(frm, cdt, cdn) {
+        frappe.msgprint('A row has been added to the links table 🎉 ');
+    }
+});
 
-	// }
+frappe.ui.form.on('Periodo Pago', {
+    before_load(frm) {
+        console.log("before_load");
+        console.log(frm);
+        // write setup code
+    }
+});
+
+frappe.ui.form.on("Medicion", "water_meter", function(frm, cdt, cdn) { // notice the presence of cdt and cdn
+    console.log('water_meter');
 });
